a606216b1581f92157d7998e5971be69861b8ead
joooosamuel <jsm988@uowmail.edu.au>
2024-10-16 16:29:42 +0400

    Add files via upload

--- LAB2.c
+++ LAB2.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 /*ESPRESSO INGREDIENT*/
 #define e_c 8
 #define e_w 30
 #define e_m 0
 #define e_choc 0
-#define e_p 3.5
+
 
 /*CAPPUCCINO INGREDIENTS*/
 #define c_c 8
 #define c_w 30
 #define c_m 70
 #define c_cho 0
-#define c_p 4.5
+
 
 /*MOCHA INGREDIENTS*/
 #define m_c 8
 #define m_w 39
 #define m_m 160
 #define m_cho 30
-#define m_p 5.5
+
 
 /*Minimum for espresso*/
 #define e_c_mini 8
@@ -41,6 +43,13 @@
 #define m_m_mini 160
 #define m_cho_mini 30
 
+/*Admin Password*/
+#define pass 0000
+
+/*Prices*/
+float e_p = 3.5;
+float c_p= 4.5;
+float m_p= 5.5;
 
 
 float total_amount;
@@ -55,32 +64,35 @@ float price;
 
 int menu();
 int payment();
-
+int admin();
+int repleninshing();
+int reset();
 
 /*Main Menu*/
 int choice = 0;
-int main()
-{
-    printf("1. Order a coffee\n");
-    printf("2. Admin mode\n");
-    printf("3. Leave\n");
+int main(){
+    while (1){
+        printf("1. Order a coffee\n");
+        printf("2. Admin mode\n");
+        printf("3. Leave\n");
 
-    printf("Enter your chosen field's number from 1-3: ");
-    scanf("%d", &choice);
-
-
-    while (choice != 1 && choice != 2 && choice !=3)
-    {
-        printf("Wrong Input\n"); 
         printf("Enter your chosen field's number from 1-3: ");
         scanf("%d", &choice);
-    }
+        while (choice != 1 && choice != 2 && choice !=3){
+            printf("Wrong Input\n"); 
+            printf("Enter your chosen field's number from 1-3: ");
+            scanf("%d", &choice);
+         }
 
-    if(choice==1)
-    {
-        menu();
-    }
+        if(choice==1){
+            menu();
+        }else if (choice==2){
+            admin();
+        }else if (choice==3){
+            break;
+        }
     return 0;
+    }
 }
 
 /*If option 1 coffe is selected*/
@@ -90,11 +102,12 @@ int menu(){
     printf("1. Espresso AED %.2f\n", e_p);
     printf("2. Cappucino AED %.2f \n", c_p);
     printf("3. Mocha AED %.2f \n", m_p);
+    printf("4. Main menu \n");
 
     
-    printf("Enter your chosen field's number from 1-3: ");
+    printf("Enter your chosen field's number from 1-4: ");
     scanf("%d", &coffe_type_choice);
-    while (coffe_type_choice != 1 && coffe_type_choice != 2 && coffe_type_choice !=3)
+    while (coffe_type_choice != 1 && coffe_type_choice != 2 && coffe_type_choice !=3 && coffe_type_choice !=4)
     {
         printf("Wrong Input\n"); 
         scanf("%d", &coffe_type_choice);
@@ -110,14 +123,12 @@ int menu(){
             printf("Espresso Coffee is availble, your total is AED %.2f\n", e_p);
             coffee_beans=coffee_beans-e_c;
             water=water-e_w;
-            printf("c: %d w: %d m: %d s: %d \n ", coffee_beans, water, milk, syrup);
             printf("Do you want to confirm your order\n");
             printf("1. Yes \n");
             printf("2. No \n");
             printf("Enter your chosen field's number from 1-2: ");
             scanf("%d", &confirmation);
-            if (confirmation==1)
-            {
+            if (confirmation==1){
                 payment();
             }else if (confirmation!=1){
                 menu();
@@ -137,7 +148,6 @@ int menu(){
             coffee_beans=coffee_beans-c_c;
             water=water-c_w;
             milk=milk-c_m;
-            printf("c: %d w: %d m: %d s: %d \n", coffee_beans, water, milk, syrup);
             printf("Do you want to confirm your order\n");
             printf("1. Yes \n");
             printf("2. No \n");
@@ -165,7 +175,6 @@ int menu(){
             water=water-m_w;
             milk=milk-m_m;
             syrup=syrup-m_cho;
-            printf("c: %d w: %d m: %d s: %d \n", coffee_beans, water, milk, syrup);
             printf("Do you want to confirm your order\n");
             printf("1. Yes \n");
             printf("2. No \n");
@@ -181,6 +190,9 @@ int menu(){
 
         }
     }
+    else if(coffe_type_choice==4){
+        main();
+    }
     
     return 0;
     
@@ -190,21 +202,22 @@ int menu(){
 /*Checking if the user pays the correct amount*/
 float money_input;
 float money_total=0;
+float money_for_coffee;
+float change;
 int payment(){
     if (coffe_type_choice==1){
-        total_amount=e_p;
-        while (money_total!=total_amount)
+        money_for_coffee=e_p;
+        while (money_total!=money_for_coffee)
         {
             printf("Enter only 1 or 0.5 to pay: ");
             scanf("%f", &money_input);
-            if(money_input==1 || money_input==0.5)
-            {
+            if(money_input==1 || money_input==0.5){
                 money_total=money_input+money_total;
             }else printf("Wrong input ");
         }
     }else if (coffe_type_choice==2){
-        total_amount=c_p;
-        while (money_total!=total_amount)
+        money_for_coffee=c_p;
+        while (money_total!=money_for_coffee)
         {
             printf("Enter only 1 or 0.5 to pay: ");
             scanf("%f", &money_input);
@@ -214,8 +227,8 @@ int payment(){
             }else printf("Wrong input ");
         }
     } else if (coffe_type_choice==3){
-        total_amount=m_p;
-        while (money_total!=total_amount)
+        money_for_coffee=m_p;
+        while (money_total!=money_for_coffee)
         {
             printf("Enter only 1 or 0.5 to pay: ");
             scanf("%f", &money_input);
@@ -225,10 +238,114 @@ int payment(){
             }else printf("Wrong input ");
         }
     }
-    if (total_amount==money_total)
+    if (money_for_coffee==money_total)
     {
-        printf("Enjoy your cofee");
+        printf("Enjoy your coffee\n\n\n");
+        total_amount=total_amount+money_for_coffee;
+        menu();
     }
     
 }
 
+/*Admin Mode*/
+int admin_pass;
+int admin_choice;
+int replenish_choice;
+int price_upd_choice;
+float new_price;
+int admin(){
+    printf("Enter your password: ");
+    scanf("%d", &admin_pass);
+    if(admin_pass==pass){
+        printf("1. Display the quantity of each ingredient in the machine and the total sale amount\n");
+        printf("2. Replenish ingredients in the machine\n");
+        printf("3. Change coffee price\n");
+        printf("0. Exit the Admin function Mode\n");
+        printf("Enter your chosen field from 0-3: ");
+        scanf("%d", &admin_choice);
+        if(admin_choice==0){
+            main();
+        }else if(admin_choice==1){
+            printf("Coffee left: %d\n ", coffee_beans);
+            printf("Water left: %d\n ", water);
+            printf("Milk left: %d\n ", milk);
+            printf("Syrup left: %d\n ", syrup);
+            printf("Total Sales: %.2f\n ", total_amount);
+            reset();
+        }else if(admin_choice==2){
+            printf("Choose the item you want to replenish\n");
+            printf("1. Coffee \n");
+            printf("2. Water \n");
+            printf("3. Milk \n");
+            printf("4. Syrup \n");
+            printf("Enter your chosen option from 1-4: ");
+            scanf("%d",&replenish_choice);
+            repleninshing();
+
+        }else if (admin_choice==3){
+            printf("Choose the item you want to re-price\n");
+            printf("1. Espresso \n");
+            printf("2. Cappucino \n");
+            printf("3. Mocha \n");
+            printf("Enter your chosen field from 1-3: ");
+            scanf("%d",&price_upd_choice);
+            if (price_upd_choice==1)
+            {
+                printf("Enter the new price for the Espresso: ");
+                scanf("%f", &new_price);
+                e_p=new_price;
+            }else if (price_upd_choice==2){
+                printf("Enter the new price for the Cappucino: ");
+                scanf("%f", &new_price);
+                c_p=new_price;
+            }else if (price_upd_choice==3){
+                printf("Enter the new price for the Mocha: ");
+                scanf("%f", &new_price);
+                m_p=new_price;
+            }
+        }
+    }else {
+        printf("INCORRECT PASSWORD\n");
+        main();
+        }
+}
+
+
+
+/*Replenishing Function*/
+int replenishing_amt;
+int repleninshing(){
+    if(replenish_choice==1){
+        coffee_beans=coffee_beans+replenishing_amt;
+        printf("Total amount of coffee beans: %d",coffee_beans);
+    }else if(replenish_choice==2){
+        printf("Enter the amount of water to add in ml: ");
+        scanf("%d",&replenishing_amt);
+        printf("test");
+        water = water+replenishing_amt;
+        printf("Total amount of water: %d",water);
+    }else if(replenish_choice==3){
+        printf("Enter the amount of milk to add in ml: ");
+        scanf("%d",&replenishing_amt);
+        milk=milk+replenishing_amt;
+        printf("Total amount of milk: %d",milk);
+    }else if(replenish_choice==4){
+        printf("Enter the amount of syrup to add in ml: ");
+        scanf("%d",&replenishing_amt);
+        syrup=syrup+replenishing_amt;
+        printf("Total amount of syrup: %d",syrup);
+    }
+}
+
+
+/*Reseting the total amount*/
+int choice_reset;
+int reset(){
+    printf("Enter 1 to reset the total ");
+    scanf("%d", &choice_reset);
+    if (choice_reset==1){
+        total_amount=0;
+    }else{
+        main();
+    }
+}
